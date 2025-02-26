2d1e10fa5c3898f371d2d70312d78cafe6fed8bb
Kumar Pushpak <pushpak.chas@gmail.com>
2025-02-26 17:28:53 +0530

    Added my python script

---
+++ Cleaning_data.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 12:39:53 2025
+
+@author: pushp
+"""
+
+import pandas as pd
+#import csv file to a dataframe
+df = pd.read_csv('data.csv')
+df.dropna(inplace = True)
+new_df = df.dropna()
+print(df)
+
+
